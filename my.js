514bc68901e9263bd357c17f77077e52a736d4f2
akshaysabale07 <68426069+akshaysabale07@users.noreply.github.com>
2020-08-13 17:49:46 +0530

    Add files via upload

---
+++ my.js
@@ -0,0 +1,8 @@
+function compute(){
+    var x= Number(document.getElementById('n1').value);
+    var y=Number(document.getElementById('n2').value);
+    var z=0;
+    if(document.getElementById('add')){
+    z=x+y;
+}
+}
